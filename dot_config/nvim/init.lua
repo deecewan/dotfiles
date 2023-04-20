361f3175cf2e1271c8d6c9b94bc8fc6eba4dd6b3
David Buchan-Swanson <david.buchanswanson@ferocia.com.au>
2023-04-20 15:22:06 +1000

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -118,6 +118,9 @@ require("packer").startup(function(use)
 			vim.keymap.set("n", "<leader>ff", find_files, { desc = "find files" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "rg" })
 			vim.keymap.set("n", "<leader>fh", find_hidden, { desc = "find hidden files" })
+			vim.keymap.set("n", "<leader>fm", function()
+				builtin.man_pages({ sections = { "ALL" } })
+			end, { desc = "Open the manual" })
 			vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "rg for string under cursor" })
 		end,
 		keys = {
@@ -192,7 +195,11 @@ require("packer").startup(function(use)
 					type = "cppdbg",
 					request = "launch",
 					program = function()
-						return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+						return vim.fn.input({
+							prompt = "Path to executable: ",
+							default = vim.fn.getcwd() .. "/",
+							completion = "file",
+						})
 					end,
 					cwd = "${workspaceFolder}",
 					stopAtEntry = true,
@@ -442,36 +449,6 @@ require("packer").startup(function(use)
 		requires = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local null_ls = require("null-ls")
-			local async_formatting = function(bufnr)
-				bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-				vim.lsp.buf_request(
-					bufnr,
-					"textDocument/formatting",
-					vim.lsp.util.make_formatting_params({}),
-					function(err, res, ctx)
-						if err then
-							local err_msg = type(err) == "string" and err or err.message
-							-- you can modify the log message / level (or ignore it completely)
-							vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-							return
-						end
-
-						-- don't apply results if buffer is unloaded or has been modified
-						if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-							return
-						end
-
-						if res then
-							local client = vim.lsp.get_client_by_id(ctx.client_id)
-							vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-							vim.api.nvim_buf_call(bufnr, function()
-								vim.cmd("silent noautocmd update")
-							end)
-						end
-					end
-				)
-			end
 
 			null_ls.setup({
 				debug = true,
@@ -482,7 +459,6 @@ require("packer").startup(function(use)
 							group = augroup,
 							buffer = bufnr,
 							callback = function()
-								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
 								vim.lsp.buf.format({
 									bufnr = bufnr,
 									filter = function(format_client)
