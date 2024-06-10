1fe1125e49279e2434f48d4420de4e94ac38d8e4
David Buchan-Swanson <david.buchanswanson@ferocia.com.au>
2024-06-10 19:38:27 +1000

    Update .config/nvim/lua/plugins/_trouble.lua

--- dot_config/nvim/lua/plugins/_trouble.lua
+++ dot_config/nvim/lua/plugins/_trouble.lua
@@ -1,40 +1,40 @@
+local trouble = require("trouble")
+
 return {
 	"folke/trouble.nvim",
 	-- "~/projects/folke/trouble.nvim",
-  lazy = true,
+	lazy = true,
 	dependencies = "kyazdani42/nvim-web-devicons",
 	cmd = { "Trouble" },
-  keys = {
-    "<leader>t",
-    "gd",
-    "gR",
-  },
+	keys = {
+		"<leader>t",
+		"gd",
+		"gR",
+	},
 	config = function()
-		local trouble = require("trouble")
+		trouble.setup()
 
-		trouble.setup({
-			auto_close = true,
-			mode = "document_diagnostics",
-			auto_jump = { "lsp_definitions" },
-		})
+    ---@param opts string | table
+    ---@return function
+		local toggle_trouble = function(opts)
+      local config
+      if type(opts) == "string" then
+        config = { mode = opts }
+      else
+        config = opts
+      end
 
-		local toggle_mode = function(mode)
+
 			return function()
-				trouble.toggle({ mode = mode })
+				trouble.toggle(config)
 			end
 		end
 
-		vim.keymap.set("n", "<leader>tt", trouble.toggle, { desc = "toggle trouble" })
-		vim.keymap.set(
-			"n",
-			"<leader>tw",
-			toggle_mode("workspace_diagnostics"),
-			{ desc = "toggle workspace diagnostics" }
-		)
-		vim.keymap.set("n", "<leader>td", toggle_mode("document_diagnostics"), { desc = "toggle document diagnostics" })
-		vim.keymap.set("n", "<leader>tq", toggle_mode("quickfix"), { desc = "toggle quickfix" })
-		vim.keymap.set("n", "<leader>tl", toggle_mode("loclist"), { desc = "toggle loclist" })
-		vim.keymap.set("n", "gR", toggle_mode("lsp_references"), { desc = "show lsp references" })
-		vim.keymap.set("n", "gd", toggle_mode("lsp_definitions"), { desc = "show lsp definitions" })
+		vim.keymap.set("n", "<leader>tt", toggle_trouble("diagnostics"), { desc = "toggle trouble workspace diagnostics" })
+		vim.keymap.set("n", "<leader>td", toggle_trouble({ mode = "diagnostics", filter = { buf = 0 } }), { desc = "toggle document diagnostics" })
+		vim.keymap.set("n", "<leader>tq", toggle_trouble("quickfix"), { desc = "toggle quickfix" })
+		vim.keymap.set("n", "<leader>tl", toggle_trouble("loclist"), { desc = "toggle loclist" })
+		vim.keymap.set("n", "gR", toggle_trouble("lsp_references"), { desc = "show lsp references" })
+		vim.keymap.set("n", "gd", toggle_trouble("lsp_definitions"), { desc = "show lsp definitions" })
 	end,
 }
