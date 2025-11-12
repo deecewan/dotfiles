999a488562506df5ab4c817a4607a22c7ad56f95
David Buchan-Swanson <david.buchanswanson@ferocia.com.au>
2025-11-12 14:41:45 +1100

    Update .config/nvim/lua/plugins/_telescope.lua

--- dot_config/nvim/lua/plugins/_telescope.lua
+++ dot_config/nvim/lua/plugins/_telescope.lua
@@ -1,51 +1,61 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "master",
-	dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
-	config = function()
-		local telescope = require("telescope")
-		telescope.setup()
-		telescope.load_extension("fzf")
+  "nvim-telescope/telescope.nvim",
+  branch = "master",
+  dependencies = { { "nvim-lua/plenary.nvim" }, { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+  config = function()
+    local telescope = require("telescope")
+    telescope.setup()
+    telescope.load_extension("fzf")
 
-		local builtin = require("telescope.builtin")
+    local builtin = require("telescope.builtin")
 
-		local find_files = function()
-			vim.fn.system("git rev-parse --is-inside-work-tree")
-			if vim.v.shell_error == 0 then
-				builtin.git_files({ show_untracked = true, cwd = vim.fn.getcwd() })
-			else
-				builtin.find_files({})
-			end
-		end
+    local find_files = function()
+      vim.fn.system("git rev-parse --is-inside-work-tree")
+      if vim.v.shell_error == 0 then
+        builtin.git_files({ show_untracked = true, cwd = vim.fn.getcwd() })
+      else
+        builtin.find_files({})
+      end
+    end
 
-		local find_hidden = function()
-			builtin.find_files({
-				find_command = { "rg", "--files", "--no-ignore", "--hidden", "--glob", "!**/.git/*" },
-			})
-		end
+    local find_hidden = function()
+      builtin.find_files({
+        find_command = { "rg", "--files", "--no-ignore", "--hidden", "--glob", "!**/.git/*" },
+      })
+    end
 
-		local find_string = function()
-			-- local input = vim.fn.input("Search > ")
+    local find_string = function()
+      -- local input = vim.fn.input("Search > ")
 
-			builtin.live_grep({ debounce = 1000 })
-		end
+      builtin.live_grep({ debounce = 1000 })
+    end
 
-		vim.keymap.set("n", "<leader>fa", builtin.resume, { desc = "resume last picker" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "show buffers" })
-		vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "show all commands" })
-		vim.keymap.set("n", "<leader>ff", find_files, { desc = "find files" })
-		-- vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "rg" })
-		vim.keymap.set("n", "<leader>fg", find_string, { desc = "rg" })
-		vim.keymap.set("n", "<leader>fh", find_hidden, { desc = "find hidden files" })
-		vim.keymap.set("n", "<leader>fm", function()
-			builtin.man_pages({ sections = { "ALL" } })
-		end, { desc = "Open the manual" })
-		vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "rg for string under cursor" })
-	end,
-	keys = {
-		"<leader>f",
-	},
-	command = {
-		"Telescope",
-	},
+    local find_string_with_hidden = function()
+      builtin.live_grep({
+        additional_args = { "--hidden" },
+      })
+    end
+
+    -- create a telescope mapping that, in normal mode, when `h` is pressed,
+    -- adds the `--hidden` flag to the current live_grep search
+
+    vim.keymap.set("n", "<leader>fa", builtin.resume, { desc = "resume last picker" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "show buffers" })
+    vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "show all commands" })
+    vim.keymap.set("n", "<leader>ff", find_files, { desc = "find files" })
+    -- vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "rg" })
+    vim.keymap.set("n", "<leader>fg", find_string, { desc = "rg" })
+    vim.keymap.set("n", "<leader>fn", find_string_with_hidden, { desc = "rg --hidden" })
+    vim.keymap.set("n", "<leader>fh", find_hidden, { desc = "find hidden files" })
+    vim.keymap.set("n", "<leader>fm", function()
+      builtin.man_pages({ sections = { "ALL" } })
+    end, { desc = "Open the manual" })
+    vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "rg for string under cursor" })
+  end,
+  keys = {
+    "<leader>f",
+  },
+  command = {
+    "Telescope",
+  },
 }
