76ab41bff575cf1929aa670afb95fd49a0d782e5
David Buchan-Swanson <david.buchanswanson@ferocia.com.au>
2025-11-26 22:43:22 +1100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/_treesitter.lua

--- dot_config/nvim/lua/plugins/_treesitter.lua
+++ dot_config/nvim/lua/plugins/_treesitter.lua
@@ -1,32 +1,42 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    branch = "main",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter").install({
-        "bash",
-        "css",
-        "graphql",
-        "html",
-        "java",
-        "javascript",
-        "json",
-        "json5",
-        "jsonc",
-        "kotlin",
-        "lua",
-        "markdown",
-        "ruby",
-        "rust",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "markdown_inline",
-      })
-    end,
-  },
-  "IndianBoy42/tree-sitter-just",
+	{ "rafcamlet/nvim-luapad" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		branch = "main",
+		build = ":TSUpdate",
+		config = function()
+			local installable = {
+				"bash",
+				"css",
+				"graphql",
+				"html",
+				"java",
+				"javascript",
+				"json",
+				"json5",
+				"jsonc",
+				"kotlin",
+				"lua",
+				"markdown",
+				"ruby",
+				"rust",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+				"markdown_inline",
+			}
+
+			local to_install = {}
+			for _, lang in ipairs(installable) do
+				if not require("nvim-treesitter").get_installed(lang) then
+					table.insert(to_install, lang)
+				end
+			end
+
+			require("nvim-treesitter").install({})
+		end,
+	},
+	"IndianBoy42/tree-sitter-just",
 }
